bc4b6b16ad6408c8526f699bc9126965237222ef
julien <julien.boyer49@orange.fr>
2017-10-22 23:57:51 +0200

    Full generator done

--- src/sudoku_elements/Sudoku.js
+++ src/sudoku_elements/Sudoku.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Row from './Row';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import Generator from '../sudoku_engine/Generator';
 import Controller from '../sudoku_engine/Controller';
 
@@ -10,7 +10,7 @@ class Sudoku extends Component {
 
     this.handleRowClick = this.handleRowClick.bind(this);
     this.generatorInstance = new Generator();
-    this.state = { valuesList: this.generatorInstance.buildResponseArray() };
+    this.state = { valuesList: this.generatorInstance.buildResponseArray(), key: Array(9).fill('').map((n, i) => i) };
   }
 
   componentDidMount() {
@@ -32,28 +32,36 @@ class Sudoku extends Component {
     this.setState({
       valuesList: list
     });
+  }
 
-    let c = new Controller();
-    console.log(c.controllFullResponse(this.state.valuesList));
-    c = null;
+  call() {
+    let result = this.generatorInstance.generateVrp(60);
+
+    this.setState({
+      valuesList: result,
+      key: this.state.key.map((n, i) => result[i].join(''))
+    })
   }
 
   render() {
 
     return (
-      <Table celled>
-        <Table.Body>
-          <Row rowIndex={1} upIndexValue={this.handleRowClick} list={this.state.valuesList[0]} />
-          <Row rowIndex={2} upIndexValue={this.handleRowClick} list={this.state.valuesList[1]} />
-          <Row rowIndex={3} upIndexValue={this.handleRowClick} list={this.state.valuesList[2]} />
-          <Row rowIndex={4} upIndexValue={this.handleRowClick} list={this.state.valuesList[3]} />
-          <Row rowIndex={5} upIndexValue={this.handleRowClick} list={this.state.valuesList[4]} />
-          <Row rowIndex={6} upIndexValue={this.handleRowClick} list={this.state.valuesList[5]} />
-          <Row rowIndex={7} upIndexValue={this.handleRowClick} list={this.state.valuesList[6]} />
-          <Row rowIndex={8} upIndexValue={this.handleRowClick} list={this.state.valuesList[7]} />
-          <Row rowIndex={9} upIndexValue={this.handleRowClick} list={this.state.valuesList[8]} />
-        </Table.Body>
-      </Table>
+      <div>
+        <Button icon={'puzzle'} color={'linkedin'} label={'Generate'} onClick={() => { this.call() }} />
+        <Table celled>
+          <Table.Body>
+            <Row rowIndex={1} key={this.state.key[0]} upIndexValue={this.handleRowClick} list={this.state.valuesList[0]} />
+            <Row rowIndex={2} key={this.state.key[1]} upIndexValue={this.handleRowClick} list={this.state.valuesList[1]} />
+            <Row rowIndex={3} key={this.state.key[2]} upIndexValue={this.handleRowClick} list={this.state.valuesList[2]} />
+            <Row rowIndex={4} key={this.state.key[3]} upIndexValue={this.handleRowClick} list={this.state.valuesList[3]} />
+            <Row rowIndex={5} key={this.state.key[4]} upIndexValue={this.handleRowClick} list={this.state.valuesList[4]} />
+            <Row rowIndex={6} key={this.state.key[5]} upIndexValue={this.handleRowClick} list={this.state.valuesList[5]} />
+            <Row rowIndex={7} key={this.state.key[6]} upIndexValue={this.handleRowClick} list={this.state.valuesList[6]} />
+            <Row rowIndex={8} key={this.state.key[7]} upIndexValue={this.handleRowClick} list={this.state.valuesList[7]} />
+            <Row rowIndex={9} key={this.state.key[8]} upIndexValue={this.handleRowClick} list={this.state.valuesList[8]} />
+          </Table.Body>
+        </Table>
+      </div>
     );
   }
 }
--- src/sudoku_engine/Generator.js
+++ src/sudoku_engine/Generator.js
@@ -11,80 +11,215 @@ class Generator {
         try {
             valuesList = Array(9);
 
-            for (var i = 0; i < 9; i++)
+            for (let i = 0; i < 9; i++)
                 valuesList[i] = Array(9).fill('-');
         } catch (e) {
             console.error(e);
         }
 
-        return this.generateVrp(valuesList);
-    }
-
-    generateVrp(valuesList) {
-        let numberPerSub = Array(9).fill(9);
-        let indexes = Array(9).fill(null);
-
-        for (let i = 0; i < indexes.length; i++) {
-            let numbers = [];
-            indexes[i] = Array(numberPerSub[i]).fill(null);
-
-            indexes[i] = indexes[i].map((n) => {
-                let newInt = null;
-
-                do {
-                    newInt = this.randomNumber(9, 1);
-                } while (numbers.includes(newInt));
-
-                numbers.push(newInt);
-
-                return newInt;
-            });
-        }
-
-        valuesList = this._insertInSubGrid(valuesList, indexes);
-
         return valuesList;
     }
 
-    _insertInSubGrid(valuesList, valuesToInsert = null) {
-        if (valuesToInsert == null)
-            return valuesList;
+    generateVrp(vrpLevel) {
+        let sudoku = Array(81).fill(0)
+        let sudokuFormatted = []
+        let tempNumbers = [];
 
-        let index = 0;
-        let colum = 0;
-        let baseCol = 0;
-        let indexInsert = 0;
+        try {
+            do {
+                this.init(sudoku);
+                this.returnHiddenBlocks(sudoku, vrpLevel);
 
-        for (let j = 0; j < 9; j++) {
+                for (let l = 0; l < 81; l++) {
+                    tempNumbers.push(sudoku[l]);
 
-            for (let i = 0; i < 9; i++) {
-                let columns = this.controller.transpose(valuesList);
-                let insertion = valuesToInsert[indexInsert][valuesToInsert[indexInsert].length - 1];
-
-                if (columns[colum].includes(insertion) || valuesList[index].includes(insertion))
-                    valuesToInsert[indexInsert][valuesToInsert[indexInsert].length - 1] = '-';
-
-                valuesList[index][colum] = valuesToInsert[indexInsert].pop();
-                colum++;
-
-                if ((i + 1) % 3 === 0)
-                    colum = 0 + baseCol;
-
-                if ((i + 1) % 3 === 0)
-                    index++;
-            }
-
-            if ((j + 1) % 3 === 0) {
-                index = 0;
-                baseCol += 3;
-            }
-
-            indexInsert++;
-            colum = 0 + baseCol;
+                    if ((l + 1) % 9 === 0) {
+                        sudokuFormatted.push(tempNumbers);
+                        tempNumbers = [];
+                    }
+                }
+            } while (!this.controller.controllFullResponse(sudokuFormatted))
+        } catch (e) {
+            console.error('Stack Error: ', [e]);
+            return this.generateVrp(vrpLevel);
         }
-        
-        return valuesList;
+
+        return sudokuFormatted;
     }
+
+    returnHiddenBlocks(sudoku, n) {
+        let removable = Array(n).fill(null).map(n => this.randomNumber(81, 0));
+
+        for (let i = 0; i < n; i++)
+            sudoku[removable[i]] = '-';
+    }
+
+    returnRow(cell) {
+        return Math.floor(cell / 9);
+    }
+
+    returnCol(cell) {
+        return cell % 9;
+    }
+
+    returnBlock(cell) {
+        return Math.floor(this.returnRow(cell) / 3) * 3 + Math.floor(this.returnCol(cell) / 3);
+    }
+
+    isPossibleRow(number, row, sudoku) {
+        for (let i = 0; i <= 8; i++) {
+            if (sudoku[row * 9 + i] === number) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isPossibleCol(number, col, sudoku) {
+        for (let i = 0; i <= 8; i++) {
+            if (sudoku[col + 9 * i] === number) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isPossibleBlock(number, block, sudoku) {
+        for (let i = 0; i <= 8; i++) {
+            if (sudoku[Math.floor(block / 3) * 27 + i % 3 + 9 * Math.floor(i / 3) + 3 * (block % 3)] === number) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isPossibleNumber(cell, number, sudoku) {
+        let row = this.returnRow(cell);
+        let col = this.returnCol(cell);
+        let block = this.returnBlock(cell);
+        return this.isPossibleRow(number, row, sudoku) && this.isPossibleCol(number, col, sudoku) && this.isPossibleBlock(number, block, sudoku);
+    }
+
+    isCorrectRow(row, sudoku) {
+        let rightSequence = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        let rowTemp = [];
+        for (let i = 0; i <= 8; i++) {
+            rowTemp[i] = sudoku[row * 9 + i];
+        }
+        rowTemp.sort();
+        return rowTemp.join() === rightSequence.join();
+    }
+
+    isCorrectCol(col, sudoku) {
+        let rightSequence = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        let colTemp = [];
+        for (let i = 0; i <= 8; i++) {
+            colTemp[i] = sudoku[col + i * 9];
+        }
+        colTemp.sort();
+        return colTemp.join() === rightSequence.join();
+    }
+
+    isCorrectBlock(block, sudoku) {
+        let rightSequence = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        let blockTemp = [];
+        for (let i = 0; i <= 8; i++) {
+            blockTemp[i] = sudoku[Math.floor(block / 3) * 27 + i % 3 + 9 * Math.floor(i / 3) + 3 * (block % 3)];
+        }
+        blockTemp.sort();
+        return blockTemp.join() === rightSequence.join();
+    }
+
+    isSolvedSudoku(sudoku) {
+        for (let i = 0; i <= 8; i++) {
+            if (!this.isCorrectBlock(i, sudoku) || !this.isCorrectRow(i, sudoku) || !this.isCorrectCol(i, sudoku)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    determinePossibleValues(cell, sudoku) {
+        let possible = [];
+        for (let i = 1; i <= 9; i++) {
+            if (this.isPossibleNumber(cell, i, sudoku)) {
+                possible.unshift(i);
+            }
+        }
+        return possible;
+    }
+
+    determineRandomPossibleValue(possible, cell) {
+        let randomPicked = Math.floor(Math.random() * possible[cell].length);
+        return possible[cell][randomPicked];
+    }
+
+    scanSudokuForUnique(sudoku) {
+        let possible = [];
+        for (let i = 0; i <= 80; i++) {
+            if (sudoku[i] === 0) {
+                possible[i] = [];
+                possible[i] = this.determinePossibleValues(i, sudoku);
+                if (possible[i].length === 0) {
+                    return false;
+                }
+            }
+        }
+        return possible;
+    }
+
+    removeAttempt(attemptArray, number) {
+        let newArray = [];
+        for (let i = 0; i < attemptArray.length; i++) {
+            if (attemptArray[i] !== number) {
+                newArray.unshift(attemptArray[i]);
+            }
+        }
+        return newArray;
+    }
+
+    nextRandom(possible) {
+        let max = 9;
+        let minChoices = 0;
+        for (let i = 0; i <= 80; i++) {
+            if (possible[i] !== undefined) {
+                if ((possible[i].length <= max) && (possible[i].length > 0)) {
+                    max = possible[i].length;
+                    minChoices = i;
+                }
+            }
+        }
+        return minChoices;
+    }
+
+    init(sudoku) {
+        let saved = [];
+        let savedSudoku = [];
+        let i = 0;
+        let nextMove;
+        let whatToTry;
+        let attempt;
+        while (!this.isSolvedSudoku(sudoku)) {
+            i++;
+            nextMove = this.scanSudokuForUnique(sudoku);
+            if (nextMove === false) {
+                nextMove = saved.pop();
+                sudoku = savedSudoku.pop();
+            }
+            whatToTry = this.nextRandom(nextMove);
+            attempt = this.determineRandomPossibleValue(nextMove, whatToTry);
+            if (nextMove[whatToTry].length > 1) {
+                nextMove[whatToTry] = this.removeAttempt(nextMove[whatToTry], attempt);
+                saved.push(nextMove.slice());
+                savedSudoku.push(sudoku.slice());
+            }
+            sudoku[whatToTry] = attempt;
+        }
+
+        return sudoku;
+    }
+
 }
 
 export default Generator;
+
